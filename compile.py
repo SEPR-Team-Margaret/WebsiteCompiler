117e83589562bd69df2a228f0a2c0f5a369624c8
David Clarke <dclarkeu@gmail.com>
2018-03-24 02:59:38 +0000

    Updated the way asm pages are compiled and stored

--- compile.py
+++ compile.py
@@ -1,13 +1,26 @@
 from jinja2 import Template, FileSystemLoader
 from jinja2.environment import Environment
 import os
+import json
 
 env = Environment()
-env.loader = FileSystemLoader('source/.')
+env.loader = FileSystemLoader("source/.")
 
 for file in os.listdir("source"):
     if file.endswith(".html"):
-        template = env.get_template(file)
-        s = template.render()
-        with open("compiled/"+file, "w") as f:
-            f.write(s)
+        if file == "asm.html":
+            with open("source/asmdata.json", 'r') as d:
+                asmdata = json.load(d)
+            for asmnum, asm in enumerate(asmdata["data"]):
+                template = env.get_template(file)
+                s = template.render(asmdata=asm)
+                with open("compiled/asm"+str(asmnum+1)+".html", 'w') as f:
+                    f.write(s)
+
+            
+            
+        else:
+            template = env.get_template(file)
+            s = template.render()
+            with open("compiled/"+file, 'w') as f:
+                f.write(s)
